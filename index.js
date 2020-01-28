f07be79c3eb0a93b32a91505d0100d8f58ba0265
Lucas Nogueira <lucas_snogueira@hotmail.com>
2020-01-28 20:57:26 -0200

    criando crud

--- index.js
+++ index.js
@@ -2,12 +2,42 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
 const users = ['Flavinho', 'Luquinhas', 'Neninha'];
+
+server.get('/users', (req, res) => {
+  return res.send(users);
+});
 
 server.get('/users/:index', (req, res) => {
   const { index } = req.params;
 
   return res.json(users[index]);
+});
+
+server.post('/users', (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+  return res.json(users);
+});
+
+server.put('/users/:index', (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users[index] = name;
+
+  return res.json(users);
+});
+
+server.delete('/users/:index', (req, res) => {
+  const { index } = req.params;
+
+  users.splice(index, 1);
+
+  return res.send();
 });
 
 server.listen(3000);
