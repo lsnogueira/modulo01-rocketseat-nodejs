8ee90c3317bd74993af763ee9701b75d8946b635
Lucas Nogueira <lucas_snogueira@hotmail.com>
2020-01-28 20:18:28 -0200

    cria get de usuarios

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const server = express();
 
-server.get('/teste', (req, res) => {
-  return res.json({ message: 'assento de foguete' });
+const users = ['Flavinho', 'Luquinhas', 'Neninha'];
+
+server.get('/users/:index', (req, res) => {
+  const { index } = req.params;
+
+  return res.json(users[index]);
 });
 
 server.listen(3000);
